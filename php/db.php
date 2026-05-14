bfd32238b85d265cc5240306227f9e8d27202e3b
Tuğba Çağlar <tugbacaglarrr7@gmail.com>
2026-05-14 09:22:48 +0300

    Update: editorial design added

--- php/db.php
+++ php/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$dbname = 'portfolio_db';
-$username = 'root';
-$password = '';
+$host = 'sqlXXX.infinityfree.com'; // 
+$dbname = 'if0_41912995_tugbacaglar';
+$username = 'if0_41912995';
+$password = 'MDY0f9L9JYhic';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
